c4c445a2d26e64a889627fa050294b43645b52d8
David Ly <dly@nerdery.com>
2014-04-01 22:51:51 -0500

    Added initial migration Set up SCSS support

--- FullStopDotNet2014.Web/App_Start/BundleConfig.cs
+++ FullStopDotNet2014.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace FullStopDotNet2014.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.scss"));
         }
     }
 }
